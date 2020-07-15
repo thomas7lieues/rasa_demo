cb31e22b796b1b02554b61cff704ca5b29652081
Thomas <@7lieues.io>
2020-07-15 17:27:33 +0200

    Modification launch

--- launch.sh
+++ launch.sh
@@ -1,7 +1,3 @@
-echo "*** Upgrading pip ***"
-pip install --upgrade pip
-
-
 if [ ! -d "venv" ]; then
   echo "*** Creating virtual environment ***"
   python3 -m venv --system-site-packages ./venv
@@ -11,6 +7,9 @@ fi
 
 echo "*** Activating virtual environment ***"
 source ./venv/bin/activate
+
+echo "*** Upgrading pip ***"
+pip install --upgrade pip
 
 echo "*** Installing pip packages ***"
 pip3 install -r requirements.txt
